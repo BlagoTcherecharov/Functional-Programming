dd6fa3c7b0fe4ed6c87790397bf9a7446997a195
BlagoTcherecharov <117592898+BlagoTcherecharov@users.noreply.github.com>
2025-10-02 11:30:06 +0300

    Update Operations.cs

--- Lambda_Functions/Operations.cs
+++ Lambda_Functions/Operations.cs
@@ -10,15 +10,15 @@ namespace Operations
     {
         static void Main(string[] args)
         {
-            Action<int, int> Addition = (x, y) => { Console.WriteLine("x + y = " + (x + y)); };
-            Action<int, int> Substraction = (x, y) => { Console.WriteLine("x - y = " + (x - y)); };
-            Action<int, int> Multiplication = (x, y) => { Console.WriteLine("x * y = " + (x * y)); };
-            Action<int, int> Division = (x, y) => { if (y != 0) { Console.WriteLine("x / y = " + (x + y)); } else { Console.WriteLine("Can't divide by 0"); } };
+            Action<double, double> Addition = (x, y) => { Console.WriteLine("x + y = " + (x + y)); };
+            Action<double, double> Substraction = (x, y) => { Console.WriteLine("x - y = " + (x - y)); };
+            Action<double, double> Multiplication = (x, y) => { Console.WriteLine("x * y = " + (x * y)); };
+            Action<double, double> Division = (x, y) => { if (y != 0) { Console.WriteLine("x / y = " + (x / y)); } else { Console.WriteLine("Can't divide by 0"); } };
 
             Console.Write("Enter x: ");
-            int input_x = int.Parse(Console.ReadLine());
+            double input_x = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter y: ");
-            int input_y = int.Parse(Console.ReadLine());
+            double input_y = Convert.ToDouble(Console.ReadLine());
 
             Addition(input_x, input_y);
             Substraction(input_x, input_y);
