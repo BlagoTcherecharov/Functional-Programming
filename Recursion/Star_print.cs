2cff1b67a46fa89eb2c0226fe3698bf57a73fd6e
BlagoTcherecharov <117592898+BlagoTcherecharov@users.noreply.github.com>
2025-11-06 10:57:46 +0200

    Add recursive star printing functionality
    Implement a recursive method to print stars based on user input.

---
+++ Recursion/Star_print.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPrint
+{
+    class Program
+    {
+        static void Print(int n)
+        {
+            Console.Write("*");
+            if (n > 1)
+                Print(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            Print(x);
+        }
+    }
+}
